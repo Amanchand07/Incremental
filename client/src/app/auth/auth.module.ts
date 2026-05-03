6d8371fcde159c0fcc032011abd689353b32b090
Amanchand07 <dummy@gmail.com>
2026-05-03 10:43:04 +0000

    day21

--- client/src/app/auth/auth.module.ts
+++ client/src/app/auth/auth.module.ts
@@ -4,17 +4,23 @@ import { CommonModule } from "@angular/common";
 import { AuthRoutingModule } from "./auth-routing.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { AuthComponent } from "./auth.component";
+import { LoginComponent } from "./components/login/login.component";
+import { UserComponent } from "./components/user/user.component";
+import { LogoutComponent } from "./components/logout/logout.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [],
+  declarations: [AuthComponent, LoginComponent, UserComponent, LogoutComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RouterModule
   ],
   exports: [
-    
+      LogoutComponent
   ]
 })
-export class AuthModule {}
+export class AuthModule { }
